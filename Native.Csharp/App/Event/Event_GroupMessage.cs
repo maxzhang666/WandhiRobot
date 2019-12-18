966669a577cd4ecff248d8bf1d9e098363bb136b
MaxZhang <373884384@qq.com>
2019-12-18 17:51:30 +0800

    重新上传

---
+++ Native.Csharp/App/Event/Event_GroupMessage.cs
@@ -0,0 +1,20 @@
+﻿using Native.Csharp.Sdk.Cqp.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Native.Csharp.App.Event
+{
+    public class Event_GroupMessage : Native.Csharp.Sdk.Cqp.Interface.IReceiveGroupMessage
+    {
+        public void ReceiveGroupMessage(object sender, CqGroupMessageEventArgs e)
+        {
+            if (e.FromQQ.ToString() == "373884384")
+            {
+                Common.CqApi.SendGroupMessage(e.FromGroup, e.Message);
+            }
+        }
+    }
+}
